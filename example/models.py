d04bc1ffef832e013def3d074149584a84003785
Mallika Sinha <msinha034@gmail.com>
2018-06-20 10:00:12 +0530

    create-models

--- example/models.py
+++ example/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+import datetime
 
 class Company(models.Model):
     name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    date_created = models.DateField(("Date"), default=datetime.date.today)
 
     def __str__(self):
         return self.name
 
 class Language(models.Model):
     name = models.CharField(max_length = 20)
+    creator = models.CharField(max_length = 20)
+    paradigm = models.CharField(max_length = 20)
+    date_created = models.DateField(("Date"), default=datetime.date.today)
 
     def __str__(self):
         return self.name
@@ -15,7 +21,9 @@ class Language(models.Model):
 #programmmers knows many langaygye
 class Programmer(models.Model):
     name = models.CharField(max_length = 20)
+    age = models.IntegerField()
     company = models.ForeignKey(Company, on_delete = models.CASCADE)
     languages = models.ManyToManyField(Language)
+
     def __str__(self):
         return self.name
